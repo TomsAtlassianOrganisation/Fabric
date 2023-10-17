c4e9d7a4a67c7cdd8b4404fe7fcd5a3e147e1abb
tomslab <highfieldstom@gmail.com>
2023-10-17 11:58:06 +0100

    Create bigquery.tf

---
+++ bigquery.tf
@@ -0,0 +1,11 @@
+resource "google_bigquery_dataset" "allowed_3" {
+  dataset_id                  = "example_dataset3"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+  access {
+    role          = "OWNER"
+    special_group = "allUsers"
+  }
+}
